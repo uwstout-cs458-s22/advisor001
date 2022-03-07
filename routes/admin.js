e3c3d6d33a3cd6378ef43ddf4fc8a0a4d76c4c0f
Hovland-426 <84932449+Hovland-426@users.noreply.github.com>
2022-03-07 12:02:00 -0600

    Add User modal revisions, Back end connection attempt
    Revisions to Add User modal including radio buttons and a drop-down menu.
    Also initial, unfinished attempt at creating a function to connect it to back-end.

--- routes/admin.js
+++ routes/admin.js
@@ -25,6 +25,22 @@ module.exports = function () {
       next(error);
     }
   });
-
-  return router;
+  //Post function for Add User Modal
+  router.get('/users/add', isUserLoaded, async (req, res, next) =>{
+  try {
+    //Copied and pasted from above try-catch.
+    //Response: once done, request, grabbing things 
+    //Request data from modal
+    //Where 0 is will be userID, where 10000000 is will be email.
+    //Will have to pass userId and email to get to be able to use them. How to pull out of form/ modal?
+    const users = await User.create(req.session.session_token, 0, 10000000);
+    ;
+    log.info(
+      `${req.method} ${req.originalUrl} success: new user(s) info has been entered.`
+    );
+  } catch (error) {
+    next(error);
+  }
+});
+return router;
 };
