ca0478e0394c9152b36c8b6bcb49d47fcc906693
Jeremy Zyduck <zyduck.jeremy@gmail.com>
2022-04-13 23:10:57 -0500

    Creation of term model, serializer, controller, and tests for controller, created add term button and modal

---
+++ models/Term.js
@@ -0,0 +1,12 @@
+class Term {
+  constructor(params) {
+    this.id = params.id;
+    this.title = params.title;
+    this.startyear = params.startyear;
+    this.semester = params.semester;
+  }
+
+  // future complex logic functions can go here
+}
+
+module.exports = Term;
