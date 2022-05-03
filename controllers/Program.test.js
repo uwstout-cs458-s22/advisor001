557e114fa5528398f29c01e5d5449d11f342d3f3
Hovland-426 <84932449+Hovland-426@users.noreply.github.com>
2022-05-03 00:08:24 -0500

    Added test, need to be revised
    Added a couple delete test that still need to be revised.

--- controllers/Program.test.js
+++ controllers/Program.test.js
@@ -102,3 +102,34 @@ describe('fetchAll tests', () => {
     expect(axios.get).toHaveBeenCalledWith('program?limit=100&offset=0');
   });
 });
+
+describe('delete tests', () => {
+  test('delete - valid delete', async () => {
+      const Program = {
+          id: '1',
+          title: 'Computer Science',
+          description: 'This course is for students who want to learn how to program computers.',
+      };
+      axios.delete.mockResolvedValueOnce({
+          data: Program,
+          status: 200,
+      });
+
+      const result = await Program.deleteProgram('mZAYn5aLEqKUlZ_Ad9U_fWr38GaAQ1oFAhT8ds245v7Q', '1');
+
+      expect(axios.delete).toHaveBeenCalledWith('program/1');
+      expect(result.status).toEqual(200);
+  });
+
+  test('delete - Required Parameters Missing', async () => {
+      axios.delete.mockResolvedValueOnce({
+          status: 400,
+          data: { Error: 'Required Parameters Missing' },
+      });
+
+      await expect(
+          Program.deleteProgram('mZAYn5aLEqKUlZ_Ad9U_fWr38GaAQ1oFAhT8ds245v7Q', undefined)
+      ).rejects.toThrow('Error 400: Required Parameters Missing');
+      expect(axios.delete).toHaveBeenCalledWith('program/' + undefined);
+  });
+});
